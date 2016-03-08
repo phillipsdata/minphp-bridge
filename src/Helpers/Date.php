1214a73800734fff18da079c05eeb6879c0ca250
clphillips <cody@phillipsdata.com>
2016-03-08 13:43:25 -0800

    Added Date.

---
+++ src/Helpers/Date.php
@@ -0,0 +1,15 @@
+<?php
+
+use Minphp\Bridge\Initializer;
+use Minphp\Date\Date as MinphpDate;
+
+/**
+ * Date Bridge
+ *
+ * Intended for legacy backwards compatibility ONLY.
+ * Use Minphp\Date\Date instead.
+ */
+class Date extends MinphpDate
+{
+
+}
