64afffb1690df566d0f363375f9aacd941cc2fb7
clphillips <cody@phillipsdata.com>
2016-03-08 14:04:45 -0800

    Added Pagination.

---
+++ src/Helpers/Pagination.php
@@ -0,0 +1,14 @@
+<?php
+
+use Minphp\Html\Pagination as MinphpPagination;
+
+/**
+ * Pagination Bridge
+ *
+ * Intended for legacy backwards compatibility ONLY.
+ * Use Minphp\Pagination\Pagination instead.
+ */
+class Pagination extends MinphpPagination
+{
+
+}
