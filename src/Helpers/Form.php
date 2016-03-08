a17847856812af915bfee6ded4b100d4ff6bd09b
clphillips <cody@phillipsdata.com>
2016-03-08 13:46:33 -0800

    Added Form.

---
+++ src/Helpers/Form.php
@@ -0,0 +1,15 @@
+<?php
+
+use Minphp\Bridge\Initializer;
+use Minphp\Form\Form as MinphpForm;
+
+/**
+ * Form Bridge
+ *
+ * Intended for legacy backwards compatibility ONLY.
+ * Use Minphp\Form\Form instead.
+ */
+class Form extends MinphpForm
+{
+
+}
